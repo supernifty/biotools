ff948fa5ebd94b06d8ddb08419ed8ac8ba9fdc8b
supernifty <peter@supernifty.org>
2022-10-17 17:12:50 +1100

    new tools

---
+++ biotools/find_variants.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+'''
+  make a table checking for specific variants
+'''
+
+import argparse
+import collections
+import csv
+import gzip
+import logging
+import sys
+
+import cyvcf2
+
+def skip_header(fn):
+  for line in gzip.open(fn, 'rt'):
+    if not line.startswith('#'):
+      yield line
+
+def main(vcfs, mafs, variants_fn, padding):
+  logging.info('reading %s...', variants_fn)
+
+  # variants to find
+  variants = {}
+  for r in csv.DictReader(open(variants_fn, 'rt'), delimiter='\t'): # expect chrom pos ref alt
+    key = (r['chrom'], r['pos'], r['ref'], r['alt'])
+    variants[key] = 0
+    if padding > 0:
+      for p in range(padding + 1):
+        newpos = int(r['pos']) + p
+        key = (r['chrom'], str(newpos), r['ref'], r['alt'])
+        variants[key] = p
+        newpos = int(r['pos']) - p
+        key = (r['chrom'], str(newpos), r['ref'], r['alt'])
+        variants[key] = -p
+
+  logging.info('checking for %i variants in total', len(variants))
+
+  found = collections.defaultdict(set)
+
+  # each vcf
+  for vcf in vcfs:
+    logging.info('reading %s...', vcf)
+    last_chr = None
+    for variant in cyvcf2.VCF(vcf):
+      key = (variant.CHROM, str(variant.POS), variant.REF, variant.ALT[0])
+      if key in variants:
+        found[vcf].add(key)
+        logging.debug('found %s in %s', key, vcf)
+      if variant.CHROM != last_chr:
+        last_chr = variant.CHROM
+        logging.debug('chromosome %s', last_chr)
+
+  # Chromosome      Start_Position  End_Position    Strand  Variant_Classification  Variant_Type    Reference_Allele        Tumor_Seq_Allele1
+  found_count = 0
+  all_mafs = set()
+  for maf in mafs:
+    logging.info('reading %s...', maf)
+    for idx, row in enumerate(csv.DictReader(skip_header(maf), delimiter='\t')):
+      key = (row['Chromosome'], row['Start_Position'], row['Reference_Allele'], row['Tumor_Seq_Allele2'])
+      where = '{}-{}'.format(maf, row['Tumor_Sample_Barcode'])
+      all_mafs.add(where)
+      if key in variants:
+        found[where].add(key)
+        found_count += 1
+        logging.debug('found %s in %s', key, where)
+      if idx % 10000 == 0:
+        logging.debug('processed %i variants found %i last key %s', idx, found_count, key)
+
+  logging.info('writing to stdout...')
+  ofh = csv.DictWriter(sys.stdout, delimiter='\t', fieldnames=['Filename'] + ['/'.join(list(v)) for v in sorted(variants)])
+  ofh.writeheader()
+  for vcf in vcfs:
+    r = {'Filename': vcf}
+    for v in variants:
+      k = '/'.join(list(v))
+      if v in found[vcf]:
+        r[k] = 'Present'
+      else:
+        r[k] = 'Absent'
+    ofh.writerow(r)
+
+  for maf in all_mafs:
+    r = {'Filename': maf}
+    for v in variants:
+      k = '/'.join(list(v))
+      if v in found[maf]:
+        r[k] = 'Present'
+      else:
+        r[k] = 'Absent'
+    ofh.writerow(r)
+  
+  logging.info('done')
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='find variants')
+  parser.add_argument('--vcfs', required=False, nargs='*', default=[], help='vcfs to check')
+  parser.add_argument('--mafs', required=False, nargs='*', default=[], help='mafs to check')
+  parser.add_argument('--padding', required=False, default=1, type=int, help='file containing variants to look for')
+  parser.add_argument('--variants', required=True, help='file containing variants to look for')
+  parser.add_argument('--verbose', action='store_true', help='more logging')
+  args = parser.parse_args()
+  if args.verbose:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+  else:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+  main(args.vcfs, args.mafs, args.variants, args.padding)
---
+++ biotools/refseq_to_bed.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+'''
+  list of genes to exons from refseq
+  may need to sort after
+  e.g.
+  python refseq_to_bed.py --genes MSH2 RNF43 --refseq ~/crc/data/public_datasets/refseq.ucsc.hg19.180829.gz
+  #bin    name    chrom   strand  txStart txEnd   cdsStart        cdsEnd  exonCount       exonStarts      exonEnds        score   name2   cdsStartStat    cdsEndStat      exonFrames
+  0       NM_032291       chr1    +       66999638        67216822        67000041        67208778        25      66999638,67091529,67098752,67101626,67105459,67108492,67109226,67126195,67133212,67136677,67137626,67138963,67142686,67145360,67147551,67154830,67155872,67161116,67184976,67194946,67199430,67205017,67206340,67206954,67208755,     67000051,67091593,67098777,67101698,67105516,67108547,67109402,67126207,67133224,67136702,67137678,67139049,67142779,67145435,67148052,67154958,67155999,67161176,67185088,67195102,67199563,67205220,67206405,67207119,67216822,     0       SGIP1   cmpl    cmpl    0,1,2,0,0,0,1,0,0,0,1,2,1,1,1,1,0,1,1,2,2,0,2,1,1,
+'''
+
+import argparse
+import collections
+import csv
+import gzip
+import logging
+import sys
+
+def main(genes, transcripts, refseq):
+  logging.info('starting...')
+  genes = set(genes)
+  if transcripts is not None:
+    transcripts = set(transcripts)
+  mins = collections.defaultdict(int)
+  mins.default_factory = lambda: 3e9
+  maxs = collections.defaultdict(int)
+  for row in csv.DictReader(gzip.open(refseq, 'rt'), delimiter='\t'):
+    if (genes is not None or row['name2'] in genes) and (transcripts is None or row['name'] in transcripts):
+      for x, y in zip(row['exonStarts'].split(','), row['exonEnds'].split(',')):
+        if x != '' and y != '':
+          sys.stdout.write('{}\t{}\t{}\t{}\n'.format(row['chrom'], x, y, row['name2']))
+
+  logging.info('done')
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Generate region for specified genes')
+  parser.add_argument('--genes', required=False, nargs='+', help='genes to include')
+  parser.add_argument('--transcripts', required=False, nargs='+', help='transcripts to include')
+  parser.add_argument('--refseq', required=True, help='refseq gz')
+  parser.add_argument('--verbose', action='store_true', help='more logging')
+  args = parser.parse_args()
+  if args.verbose:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+  else:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+
+  main(args.genes, args.transcripts, args.refseq)
